731a9472179bba045a3ca710d67369119652bef3
Chandu5342 <karrichandu309@gmail.com>
2025-04-05 03:18:21 +0530

    change1

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+function showTab(tabId, event) {
+  console.log(tabId)
+  // Hide all content
+  document.querySelectorAll('.tab-content').forEach(tab => {
+      tab.classList.remove('active');
+  });
+
+  // Remove active class from all buttons
+  document.querySelectorAll('.tab').forEach(btn => {
+      btn.classList.remove('active');
+  });
+
+  // Show the selected tab
+  document.getElementById(tabId).classList.add('active');
+
+  // Highlight the clicked button
+  event.currentTarget.classList.add('active');
+}
